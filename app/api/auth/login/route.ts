1830d59eed3bbb46ec20ffed1dd19c770b4ad73d
kajumac07 <kajumac13@gmail.com>
2025-09-18 20:14:00 +0530

    changes in login route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -3,30 +3,27 @@ import clientPromise from "../../../lib/mongodb";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-interface AdminUser {
+interface User {
   _id: string;
-  name: string;
   email: string;
   passwordHash: string;
-  type: "admin";
-  createdAt: Date;
+  type: "admin" | "client" | "customer";
 }
 
 export async function POST(req: Request) {
   try {
     const { email, password } = await req.json();
-    if (!email || !password) {
+    if (!email || !password)
       return NextResponse.json(
         { error: "Email and password required" },
         { status: 400 }
       );
-    }
 
     const client = await clientPromise;
     const db = client.db(process.env.MONGODB_DB);
 
     const user = await db
-      .collection<AdminUser>("users")
+      .collection<User>("users")
       .findOne({ email, type: "admin" });
     if (!user)
       return NextResponse.json(
@@ -49,7 +46,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ token });
   } catch (err) {
-    console.error("POST /api/auth/login error:", err);
+    console.error(err);
     return NextResponse.json(
       { error: "Something went wrong" },
       { status: 500 }
